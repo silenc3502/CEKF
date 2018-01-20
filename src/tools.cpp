d0d0cc1384dc4ba2198bac68110990be701fdb7c
silenc3502 <gcccompil3r@gmail.com>
2018-01-21 08:40:41 +0900

    Add lecture code 18 to calculate Jacobian Matrix

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "tools.h"
 
+#define EPSILON		0.000001
+#define EPSILON2	do	\
+{				\
+	pow(EPSILON, 2)		\
+}				\
+while(0)
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -49,4 +56,32 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+
+  /* It comes from lecture 18 */
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  MatrixXd Hj(3, 4);
+
+  if(fabs(px) < EPSILON && fabs(py) < EPSILON)
+  {
+    px = EPSILON;
+    py = EPSILON;
+  }
+
+  float c1 = pow(px, 2) + pow(py, 2);
+
+  if(fabs(c1) < EPSILON2)
+    c1 = EPSILON2;
+
+  float c2 = sqrt(c1);
+  float c3 = (c1 * c2);
+
+  Hj << (px / c2), (py / c2), 0, 0,
+	-(py / c1), (px / c1), 0, 0,
+	py * (vx * py - vy * px) / c3, px * (px * vy - py * vx) / c3, px / c2, py / c2;
+
+  return Hj;
 }
