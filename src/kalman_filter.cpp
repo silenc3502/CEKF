526f18a80af9762f7543573d0f29f6b409595e0b
silenc3502 <gcccompil3r@gmail.com>
2018-01-21 09:02:26 +0900

    Add Kalman Filter Algorithm

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+
+  /* It comes from lecture 6 KF Prediction step */
+  x = F_ * x;
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +36,18 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+
+  /* It comes from lecture 6 KF Measurement update step */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,23 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  /* It comes from lecture 20 EKF Equations */
+  double rho = sqrt(pow(x_(0), 2) + pow(x_(1), 2));
+  double theta = atan(x_(1) / x_(0));
+  double rho_dot = (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
+
+  VectorXd h = VectorXd(3);
+  h << rho, theta, rho_dot;
+
+  VectorXd y = z - h;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
